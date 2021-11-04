0545af80fbdd2bc8bf8a45b519bcf2792772b45a
Benjamin Vickers <bvickers@vmware.com>
2021-11-04 15:57:31 +0100

    deploy project tf-test2

---
+++ deployed-projects/tf-test2/kubernetes.tf
@@ -0,0 +1,54 @@
+
+# dev 
+provider "kubernetes" {
+  config_path    = "~/.kube/config"
+  config_context = "rpi"
+  alias = "dev"
+}
+
+# acc
+provider "kubernetes" {
+  config_path    = "~/.kube/config"
+  config_context = "rpi"
+  alias = "acc"
+}
+
+# prod
+provider "kubernetes" {
+  config_path    = "~/.kube/config"
+  config_context = "rpi"
+  alias = "prod"
+}
+
+module "dev-namespace" {
+  count = "${var.enable-k8s-dev ? 1 : 0}"
+  source = "../../individual-entitlements/kubernetes-namespace"
+  project-name = "${local.project-name}-dev"
+  namespace-admins = var.users
+  cluster-context = "rpi"
+  providers = {
+    kubernetes = kubernetes.dev
+  }
+}
+
+module "test-namespace" {
+  count = "${var.enable-k8s-acc ? 1 : 0}"
+  source = "../../individual-entitlements/kubernetes-namespace"
+  project-name = "${local.project-name}-acc"
+  namespace-admins = var.users
+  cluster-context = "rpi"
+  providers = {
+    kubernetes = kubernetes.acc
+  }
+}
+
+module "prod-namespace" {
+  count = "${var.enable-k8s-prod ? 1 : 0}"
+  source = "../../individual-entitlements/kubernetes-namespace"
+  project-name = "${local.project-name}-prod"
+  namespace-admins = var.users
+  cluster-context = "rpi"
+  providers = {
+    kubernetes = kubernetes.prod
+  }
+}
