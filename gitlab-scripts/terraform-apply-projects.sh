a4e37fbedd610dd9ef7b875d755fb175ed4e21e9
Benjamin Vickers <bvickers@vmware.com>
2021-11-08 11:57:28 +0100

    testing tf commands in script

--- gitlab-scripts/terraform-apply-projects.sh
+++ gitlab-scripts/terraform-apply-projects.sh
@@ -1,3 +1,14 @@
 #!/bin/sh
-projects="$(git diff --name-only HEAD~8 | grep deployed-projects | cut -d "/" -f2 | sort | uniq)"
-printf "Will deploy the following projects with changed detected:\n${projects}"
+set -eu
+
+projects="$(git diff --name-only HEAD~12 | grep deployed-projects | cut -d "/" -f2 | sort | uniq)"
+printf "Will deploy the following projects with changes detected:\n${projects}\n"
+
+for proj in $projects; do
+  echo "placeholder: deploying $proj"
+  cd "deployed-projects/$proj"
+  ls
+  terraform init
+  terraform plan
+  cd -
+done 
