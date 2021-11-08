b1712b8e508c49877bfbd52f90434614dddd9dd7
Benjamin Vickers <bvickers@vmware.com>
2021-11-08 12:05:35 +0100

    terraform plan in script is working locally

--- gitlab-scripts/terraform-apply-projects.sh
+++ gitlab-scripts/terraform-apply-projects.sh
@@ -1,14 +1,15 @@
 #!/bin/sh
 set -eu
 
-projects="$(git diff --name-only HEAD~12 | grep deployed-projects | cut -d "/" -f2 | sort | uniq)"
+projects="$(git diff --name-only HEAD~14 | grep deployed-projects | cut -d "/" -f2 | sort | uniq)"
 printf "Will deploy the following projects with changes detected:\n${projects}\n"
 
 for proj in $projects; do
-  echo "placeholder: deploying $proj"
+  printf "\n-----------  Starting to deploy $proj  -------------\n\n"
   cd "deployed-projects/$proj"
   ls
   terraform init
   terraform plan
   cd -
+  echo "\n-----------  Finished deploying $proj  ------------\n\n"
 done 
