25ca0b6d9431eb9c7ce743ee76bc5c6fc91f1f13
Benjamin Vickers <bvickers@vmware.com>
2021-11-09 10:26:58 +0100

    fixing push script

--- push-project-changes.sh
+++ push-project-changes.sh
@@ -8,8 +8,8 @@ git add "$deploy_dir"
 git commit -m "generate project $project"
 if [ ! $(git fetch origin "$(git branch --show-current)") ]; then
   sleep 1
-  echo "no remote found; this is a new branch"
+  echo "no branch \"$(git branch --show-current)\" found at remote origin; this seems to be a new branch"
 else
   git pull -r --autostash
 fi
-git push -u origin "$(git branch --show-current)"
+git push -u origin
